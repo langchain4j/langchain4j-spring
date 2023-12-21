5a7fe990e649b735b150787c42d89be4a425f45c
LangChain4j <info@langchain4j.dev>
2023-12-21 09:11:01 +0100

    Extracted OpenAI Spring Boot starter into a separate module

---
+++ langchain4j-open-ai-spring-boot-starter/src/main/java/dev/langchain4j/openai/spring/ModerationModelProperties.java
@@ -0,0 +1,20 @@
+package dev.langchain4j.openai.spring;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Duration;
+
+@Getter
+@Setter
+class ModerationModelProperties {
+
+    String baseUrl;
+    String apiKey;
+    String organizationId;
+    String modelName;
+    Duration timeout;
+    Integer maxRetries;
+    Boolean logRequests;
+    Boolean logResponses;
+}
