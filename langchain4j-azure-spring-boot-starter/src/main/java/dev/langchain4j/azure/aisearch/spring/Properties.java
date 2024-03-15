a423557cfd611f528069242f17391f52aa77cd0e
Zhiyong Li <zhiyongli@microsoft.com>
2024-03-15 13:38:43 +0800

    Add Azure AI Search support

---
+++ langchain4j-azure-spring-boot-starter/src/main/java/dev/langchain4j/azure/aisearch/spring/Properties.java
@@ -0,0 +1,18 @@
+package dev.langchain4j.azure.aisearch.spring;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = Properties.PREFIX)
+public class Properties {
+
+    static final String PREFIX = "langchain4j.azure.ai-search";
+
+    String endpoint;
+    String apiKey;
+    int dimensions;
+    boolean setupIndex;
+}
