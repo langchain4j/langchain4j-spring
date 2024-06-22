052c49e2d2a836edb39247bae9828e8b32f23b0c
Martin7-1 <1754350460@qq.com>
2024-06-23 00:03:49 +0800

    unified IT

--- langchain4j-milvus-spring-boot-starter/src/test/java/MilvusEmbeddingStoreAutoConfigurationIT.java
+++ langchain4j-milvus-spring-boot-starter/src/test/java/MilvusEmbeddingStoreAutoConfigurationIT.java
@@ -1,30 +1,15 @@
-import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
-import dev.langchain4j.model.embedding.AllMiniLmL6V2QuantizedEmbeddingModel;
-import dev.langchain4j.model.embedding.EmbeddingModel;
-import dev.langchain4j.store.embedding.EmbeddingMatch;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.milvus.MilvusEmbeddingStore;
 import dev.langchain4j.store.embedding.milvus.spring.MilvusEmbeddingStoreAutoConfiguration;
-import lombok.SneakyThrows;
+import dev.langchain4j.store.embedding.spring.EmbeddingStoreAutoConfigurationIT;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.AutoConfigurations;
-import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.testcontainers.milvus.MilvusContainer;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.data.Percentage.withPercentage;
-
-class MilvusEmbeddingStoreAutoConfigurationIT {
+class MilvusEmbeddingStoreAutoConfigurationIT extends EmbeddingStoreAutoConfigurationIT {
 
     static MilvusContainer milvus = new MilvusContainer("milvusdb/milvus:v2.3.16");
-
-    ApplicationContextRunner contextRunner = new ApplicationContextRunner()
-            .withConfiguration(AutoConfigurations.of(MilvusEmbeddingStoreAutoConfiguration.class));
 
     @BeforeAll
     static void beforeAll() {
@@ -36,41 +21,22 @@ class MilvusEmbeddingStoreAutoConfigurationIT {
         milvus.stop();
     }
 
-    @Test
-    void should_provide_milvus_vector_store() {
-        TextSegment segment = TextSegment.from("hello");
-        contextRunner
-                .withBean(AllMiniLmL6V2QuantizedEmbeddingModel.class)
-                .withPropertyValues(
-                        "langchain4j.milvus.host=" + milvus.getHost(),
-                        "langchain4j.milvus.port=" + milvus.getMappedPort(19530),
-                        "langchain4j.milvus.retrieveEmbeddingsOnSearch=true"
-                )
-                .run(context -> {
-                    EmbeddingModel embeddingModel = context.getBean(AllMiniLmL6V2QuantizedEmbeddingModel.class);
-                    Embedding embedding = embeddingModel.embed(segment.text()).content();
-
-                    EmbeddingStore<TextSegment> embeddingStore = context.getBean(MilvusEmbeddingStore.class);
-                    assertThat(embeddingStore).isInstanceOf(MilvusEmbeddingStore.class);
-
-                    String id = embeddingStore.add(embedding, segment);
-                    assertThat(id).isNotBlank();
-
-                    awaitUntilPersisted();
-
-                    List<EmbeddingMatch<TextSegment>> relevant = embeddingStore.findRelevant(embedding, 10);
-                    assertThat(relevant).hasSize(1);
-
-                    EmbeddingMatch<TextSegment> match = relevant.get(0);
-                    assertThat(match.score()).isCloseTo(1, withPercentage(1));
-                    assertThat(match.embeddingId()).isEqualTo(id);
-                    assertThat(match.embedding()).isEqualTo(embedding);
-                    assertThat(match.embedded()).isEqualTo(segment);
-                });
+    @Override
+    protected Class<?> autoConfigurationClass() {
+        return MilvusEmbeddingStoreAutoConfiguration.class;
     }
 
-    @SneakyThrows
-    private void awaitUntilPersisted() {
-        Thread.sleep(1000);
+    @Override
+    protected Class<? extends EmbeddingStore<TextSegment>> embeddingStoreClass() {
+        return MilvusEmbeddingStore.class;
+    }
+
+    @Override
+    protected String[] properties() {
+        return new String[]{
+                "langchain4j.milvus.host=" + milvus.getHost(),
+                "langchain4j.milvus.port=" + milvus.getMappedPort(19530),
+                "langchain4j.milvus.retrieveEmbeddingsOnSearch=true"
+        };
     }
 }
