696effe8adf01afd97b6b5caacb6528def6dd1e2
Martin7-1 <1754350460@qq.com>
2024-06-22 23:57:33 +0800

    unified IT

--- langchain4j-redis-spring-boot-starter/src/test/java/dev/langchain4j/store/embedding/redis/spring/RedisEmbeddingStoreAutoConfigurationIT.java
+++ langchain4j-redis-spring-boot-starter/src/test/java/dev/langchain4j/store/embedding/redis/spring/RedisEmbeddingStoreAutoConfigurationIT.java
@@ -1,34 +1,21 @@
 package dev.langchain4j.store.embedding.redis.spring;
 
 import com.redis.testcontainers.RedisContainer;
-import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
-import dev.langchain4j.model.embedding.AllMiniLmL6V2QuantizedEmbeddingModel;
-import dev.langchain4j.model.embedding.EmbeddingModel;
-import dev.langchain4j.store.embedding.EmbeddingMatch;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.redis.RedisEmbeddingStore;
+import dev.langchain4j.store.embedding.spring.EmbeddingStoreAutoConfigurationIT;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.AutoConfigurations;
-import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.testcontainers.containers.wait.strategy.Wait;
-
-import java.util.List;
 
 import static com.redis.testcontainers.RedisStackContainer.DEFAULT_IMAGE_NAME;
 import static com.redis.testcontainers.RedisStackContainer.DEFAULT_TAG;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.data.Percentage.withPercentage;
 
-class RedisEmbeddingStoreAutoConfigurationIT {
+class RedisEmbeddingStoreAutoConfigurationIT extends EmbeddingStoreAutoConfigurationIT {
 
     static RedisContainer redis = new RedisContainer(DEFAULT_IMAGE_NAME.withTag(DEFAULT_TAG))
             .waitingFor(Wait.defaultWaitStrategy());
-
-    ApplicationContextRunner contextRunner = new ApplicationContextRunner()
-            .withConfiguration(AutoConfigurations.of(RedisEmbeddingStoreAutoConfiguration.class));
 
     @BeforeAll
     static void beforeAll() {
@@ -40,33 +27,21 @@ class RedisEmbeddingStoreAutoConfigurationIT {
         redis.stop();
     }
 
-    @Test
-    void should_provide_redis_vector_store() {
-        TextSegment segment = TextSegment.from("hello");
+    @Override
+    protected Class<?> autoConfigurationClass() {
+        return RedisEmbeddingStoreAutoConfiguration.class;
+    }
 
-        contextRunner
-                .withBean(AllMiniLmL6V2QuantizedEmbeddingModel.class)
-                .withPropertyValues(
-                        "langchain4j.redis.host=" + redis.getHost(),
-                        "langchain4j.redis.port=" + redis.getFirstMappedPort()
-                )
-                .run(context -> {
-                    EmbeddingModel embeddingModel = context.getBean(AllMiniLmL6V2QuantizedEmbeddingModel.class);
-                    Embedding embedding = embeddingModel.embed(segment.text()).content();
-                    EmbeddingStore<TextSegment> embeddingStore = context.getBean(RedisEmbeddingStore.class);
-                    assertThat(embeddingStore).isInstanceOf(RedisEmbeddingStore.class);
+    @Override
+    protected Class<? extends EmbeddingStore<TextSegment>> embeddingStoreClass() {
+        return RedisEmbeddingStore.class;
+    }
 
-                    String id = embeddingStore.add(embedding, segment);
-                    assertThat(id).isNotBlank();
-
-                    List<EmbeddingMatch<TextSegment>> relevant = embeddingStore.findRelevant(embedding, 10);
-                    assertThat(relevant).hasSize(1);
-
-                    EmbeddingMatch<TextSegment> match = relevant.get(0);
-                    assertThat(match.score()).isCloseTo(1, withPercentage(1));
-                    assertThat(match.embeddingId()).isEqualTo(id);
-                    assertThat(match.embedding()).isEqualTo(embedding);
-                    assertThat(match.embedded()).isEqualTo(segment);
-                });
+    @Override
+    protected String[] properties() {
+        return new String[]{
+                "langchain4j.redis.host=" + redis.getHost(),
+                "langchain4j.redis.port=" + redis.getFirstMappedPort()
+        };
     }
 }
