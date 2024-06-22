3fcb32f57cce7ad8feaa518976e60f725e0dd7f6
Martin7-1 <1754350460@qq.com>
2024-06-22 23:54:18 +0800

    make await implement nothing

--- langchain4j-spring-boot-core/src/test/java/dev/langchain4j/store/embedding/spring/EmbeddingStoreAutoConfigurationIT.java
+++ langchain4j-spring-boot-core/src/test/java/dev/langchain4j/store/embedding/spring/EmbeddingStoreAutoConfigurationIT.java
@@ -55,5 +55,7 @@ public abstract class EmbeddingStoreAutoConfigurationIT {
                 });
     }
 
-    protected abstract void awaitUntilPersisted();
+    protected void awaitUntilPersisted() {
+
+    }
 }
