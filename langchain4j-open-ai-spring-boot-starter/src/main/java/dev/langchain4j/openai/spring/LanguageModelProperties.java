9c1046504856e9e0aa36c318689fa92e5821012a
LangChain4j <info@langchain4j.dev>
2024-09-05 15:06:04 +0200

    OpenAI: added missing properties

--- langchain4j-open-ai-spring-boot-starter/src/main/java/dev/langchain4j/openai/spring/LanguageModelProperties.java
+++ langchain4j-open-ai-spring-boot-starter/src/main/java/dev/langchain4j/openai/spring/LanguageModelProperties.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
 import java.time.Duration;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -21,4 +22,5 @@ class LanguageModelProperties {
     ProxyProperties proxy;
     Boolean logRequests;
     Boolean logResponses;
+    Map<String, String> customHeaders;
 }
