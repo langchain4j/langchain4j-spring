fd8ed4b7fd410b85b90f914a0f2a2dcb9b2befd6
LangChain4j <info@langchain4j.dev>
2024-09-15 10:11:07 +0200

    Closes https://github.com/langchain4j/langchain4j/issues/886

---
+++ langchain4j-reactor/src/test/java/dev/langchain4j/reactor/AiServiceWithFluxTest.java
@@ -0,0 +1,39 @@
+package dev.langchain4j.reactor;
+
+import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.chat.mock.StreamingChatModelMock;
+import dev.langchain4j.service.AiServices;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
+public class AiServiceWithFluxTest {
+
+    interface Assistant {
+
+        Flux<String> stream(String userMessage);
+    }
+
+    @Test
+    void should_stream() {
+
+        // given
+        List<String> tokens = List.of("The", " capital", " of", " Germany", " is", " Berlin", ".");
+
+        StreamingChatLanguageModel model = StreamingChatModelMock.thatAlwaysStreams(tokens);
+
+        Assistant assistant = AiServices.builder(Assistant.class)
+                .streamingChatLanguageModel(model)
+                .build();
+
+        // when
+        Flux<String> flux = assistant.stream("What is the capital of Germany?");
+
+        // then
+        StepVerifier.create(flux)
+                .expectNextSequence(tokens)
+                .verifyComplete();
+    }
+}
