1b93003249f062d5250c540a533d1f1d0d6b49c1
Antoine Rey <antoine.rey@free.fr>
2024-10-29 10:23:18 +0100

    langchain4j-spring-boot-starter get rid of lombok

--- langchain4j-spring-boot-starter/src/main/java/dev/langchain4j/rag/spring/RagProperties.java
+++ langchain4j-spring-boot-starter/src/main/java/dev/langchain4j/rag/spring/RagProperties.java
@@ -1,17 +1,11 @@
 package dev.langchain4j.rag.spring;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
-@Getter
-@Setter
-@ConfigurationProperties(prefix = RagProperties.PREFIX)
-public class RagProperties {
 
+@ConfigurationProperties(prefix = RagProperties.PREFIX)
+public record RagProperties(@NestedConfigurationProperty RetrievalProperties retrieval) {
     static final String PREFIX = "langchain4j.rag";
 
-    @NestedConfigurationProperty
-    RetrievalProperties retrieval;
 }
