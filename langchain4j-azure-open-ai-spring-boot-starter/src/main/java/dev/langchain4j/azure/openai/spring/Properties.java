1ac0350ff4b57e0305d3d0b0cc58762160f99aca
Antoine Rey <antoine.rey@free.fr>
2024-11-08 09:17:14 +0100

    Get rid of lombok for langchain4j-azure-open-ai-spring-boot-sarter (#51)
    ## Issue
    
    Langchain4j project issue:
    https://github.com/langchain4j/langchain4j/issues/1636
    
    ## Change
    
    Get rid of lombok from the modules:
    `langchain4j-azure-open-ai-spring-boot-starter`.
    I'll use java `record` for class annotated with
    `@ConfigurationProperties`.
    
    ## General checklist
    - [X] There are no breaking changes
    - [ ] I have added unit and integration tests for my change
    - [ ] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [ ] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    - [ ] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [ ] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    - [ ] I have added/updated [Spring Boot
    starter(s)](https://github.com/langchain4j/langchain4j-spring) (if
    applicable)

--- langchain4j-azure-open-ai-spring-boot-starter/src/main/java/dev/langchain4j/azure/openai/spring/Properties.java
+++ langchain4j-azure-open-ai-spring-boot-starter/src/main/java/dev/langchain4j/azure/openai/spring/Properties.java
@@ -1,26 +1,22 @@
 package dev.langchain4j.azure.openai.spring;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
-@Getter
-@Setter
 @ConfigurationProperties(prefix = Properties.PREFIX)
-public class Properties {
+public record Properties(
 
+    @NestedConfigurationProperty
+    ChatModelProperties chatModel,
+
+    @NestedConfigurationProperty
+    ChatModelProperties streamingChatModel,
+
+    @NestedConfigurationProperty
+    EmbeddingModelProperties embeddingModel,
+
+    @NestedConfigurationProperty
+    ImageModelProperties imageModel
+) {
     static final String PREFIX = "langchain4j.azure-open-ai";
-
-    @NestedConfigurationProperty
-    ChatModelProperties chatModel;
-
-    @NestedConfigurationProperty
-    ChatModelProperties streamingChatModel;
-
-    @NestedConfigurationProperty
-    EmbeddingModelProperties embeddingModel;
-
-    @NestedConfigurationProperty
-    ImageModelProperties imageModel;
 }
