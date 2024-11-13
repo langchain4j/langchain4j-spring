89fa967e2056c104ed8317a3b743d7b247d2e8cb
suhassk-hash <sharmasuhas450@gmail.com>
2024-11-14 02:23:18 +0530

    Base Commit for Google Gemini AI Model

---
+++ langchain4j-google-ai-gemini/src/main/java/dev/langchain4j/googleaigemini/Langchain4jGoogleAiGeminiApplication.java
@@ -0,0 +1,13 @@
+package dev.langchain4j.googleaigemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langchain4jGoogleAiGeminiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Langchain4jGoogleAiGeminiApplication.class, args);
+    }
+
+}
---
+++ langchain4j-google-ai-gemini/src/test/java/dev/langchain4j/googleaigemini/Langchain4jGoogleAiGeminiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.langchain4j.googleaigemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Langchain4jGoogleAiGeminiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
