c4fccf581f9d7b3b8997e240c0316f7d6c54aa3a
Martin7-1 <yi.zheng.se@gmail.com>
2025-01-04 01:07:19 +0800

    Elasticsearch / Redis / Milvus: Remove lombok and small update for IT (#101)
    <!--
    Thank you so much for your contribution!
    
    Please fill in all the sections below.
    Please open the PR as a draft initially. Once it is reviewed and
    approved, we will ask you to add documentation and examples.
    Please note that PRs with breaking changes or without tests will be
    rejected.
    
    Please note that PRs will be reviewed based on the priority of the
    issues they address.
    We ask for your patience. We are doing our best to review your PR as
    quickly as possible.
    Please refrain from pinging and asking when it will be reviewed. Thank
    you for understanding!
    -->
    
    ## Issue
    <!-- Please specify the ID of the issue this PR is addressing. For
    example: "Closes #1234" or "Fixes #1234" -->
    Closes
    [langchain4j#2336](https://github.com/langchain4j/langchain4j/issues/2336)
    
    ## Change
    <!-- Please describe the changes you made. -->
    
    1. Remove `lombok` in Elasticsearch / Redis / Milvus
    2. Configure `maven-source-plugin:test-jar-no-fork` for
    `langchain4j-spring-boot-tests`, in order to see test source in
    community repo.
    3. Redis: update `prefix` parameter introducing in [this
    PR](https://github.com/langchain4j/langchain4j/pull/1981)
    
    ## General checklist
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [x] There are no breaking changes
    - [ ] I have added unit and/or integration tests for my change
    - [ ] The tests cover both positive and negative cases
    - [x] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    <!-- Before adding documentation and example(s) (below), please wait
    until the PR is reviewed and approved. -->
    - [ ] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [ ] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)

--- langchain4j-milvus-spring-boot-starter/src/main/java/dev/langchain4j/store/embedding/milvus/spring/MilvusEmbeddingStoreProperties.java
+++ langchain4j-milvus-spring-boot-starter/src/main/java/dev/langchain4j/store/embedding/milvus/spring/MilvusEmbeddingStoreProperties.java
@@ -3,14 +3,10 @@ package dev.langchain4j.store.embedding.milvus.spring;
 import io.milvus.common.clientenum.ConsistencyLevelEnum;
 import io.milvus.param.IndexType;
 import io.milvus.param.MetricType;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import static io.milvus.common.clientenum.ConsistencyLevelEnum.STRONG;
 
-@Getter
-@Setter
 @ConfigurationProperties(prefix = MilvusEmbeddingStoreProperties.PREFIX)
 public class MilvusEmbeddingStoreProperties {
 
@@ -34,4 +30,116 @@ public class MilvusEmbeddingStoreProperties {
     private Boolean retrieveEmbeddingsOnSearch;
     private Boolean autoFlushOnInsert;
     private String databaseName;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public String getCollectionName() {
+        return collectionName;
+    }
+
+    public void setCollectionName(String collectionName) {
+        this.collectionName = collectionName;
+    }
+
+    public Integer getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(Integer dimension) {
+        this.dimension = dimension;
+    }
+
+    public IndexType getIndexType() {
+        return indexType;
+    }
+
+    public void setIndexType(IndexType indexType) {
+        this.indexType = indexType;
+    }
+
+    public MetricType getMetricType() {
+        return metricType;
+    }
+
+    public void setMetricType(MetricType metricType) {
+        this.metricType = metricType;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ConsistencyLevelEnum getConsistencyLevel() {
+        return consistencyLevel;
+    }
+
+    public void setConsistencyLevel(ConsistencyLevelEnum consistencyLevel) {
+        this.consistencyLevel = consistencyLevel;
+    }
+
+    public Boolean getRetrieveEmbeddingsOnSearch() {
+        return retrieveEmbeddingsOnSearch;
+    }
+
+    public void setRetrieveEmbeddingsOnSearch(Boolean retrieveEmbeddingsOnSearch) {
+        this.retrieveEmbeddingsOnSearch = retrieveEmbeddingsOnSearch;
+    }
+
+    public Boolean getAutoFlushOnInsert() {
+        return autoFlushOnInsert;
+    }
+
+    public void setAutoFlushOnInsert(Boolean autoFlushOnInsert) {
+        this.autoFlushOnInsert = autoFlushOnInsert;
+    }
+
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
 }
--- langchain4j-redis-spring-boot-starter/src/test/java/dev/langchain4j/store/embedding/redis/spring/RedisEmbeddingStoreAutoConfigurationIT.java
+++ langchain4j-redis-spring-boot-starter/src/test/java/dev/langchain4j/store/embedding/redis/spring/RedisEmbeddingStoreAutoConfigurationIT.java
@@ -1,6 +1,6 @@
 package dev.langchain4j.store.embedding.redis.spring;
 
-import com.redis.testcontainers.RedisContainer;
+import com.redis.testcontainers.RedisStackContainer;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.redis.RedisEmbeddingStore;
@@ -8,16 +8,16 @@ import dev.langchain4j.store.embedding.spring.EmbeddingStoreAutoConfigurationIT;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.testcontainers.containers.wait.strategy.Wait;
-import redis.clients.jedis.JedisPooled;
 
 import static com.redis.testcontainers.RedisStackContainer.DEFAULT_IMAGE_NAME;
 import static com.redis.testcontainers.RedisStackContainer.DEFAULT_TAG;
+import static dev.langchain4j.internal.Utils.randomUUID;
 
 class RedisEmbeddingStoreAutoConfigurationIT extends EmbeddingStoreAutoConfigurationIT {
 
-    static RedisContainer redis = new RedisContainer(DEFAULT_IMAGE_NAME.withTag(DEFAULT_TAG))
-            .waitingFor(Wait.defaultWaitStrategy());
+    static RedisStackContainer redis = new RedisStackContainer(DEFAULT_IMAGE_NAME.withTag(DEFAULT_TAG));
+
+    String indexName;
 
     @BeforeAll
     static void beforeAll() {
@@ -30,10 +30,8 @@ class RedisEmbeddingStoreAutoConfigurationIT extends EmbeddingStoreAutoConfigura
     }
 
     @BeforeEach
-    void beforeEach() {
-        try (JedisPooled jedis = new JedisPooled(redis.getHost(), redis.getFirstMappedPort())) {
-            jedis.flushDB(); // TODO fix: why redis returns embeddings from different indexes?
-        }
+    void setIndexName() {
+        indexName = randomUUID();
     }
 
     @Override
@@ -50,7 +48,9 @@ class RedisEmbeddingStoreAutoConfigurationIT extends EmbeddingStoreAutoConfigura
     protected String[] properties() {
         return new String[]{
                 "langchain4j.redis.host=" + redis.getHost(),
-                "langchain4j.redis.port=" + redis.getFirstMappedPort()
+                "langchain4j.redis.port=" + redis.getFirstMappedPort(),
+                "langchain4j.redis.prefix=" + indexName + ":",
+                "langchain4j.redis.index-name=" + indexName
         };
     }
 
