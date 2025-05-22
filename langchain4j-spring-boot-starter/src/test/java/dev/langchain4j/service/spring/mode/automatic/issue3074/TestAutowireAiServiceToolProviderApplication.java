7f9f70699a7589dd3851ab041bcc769948b5496b
zhangzexin <zzx-nuc@qq.com>
2025-05-22 10:56:13 +0800

    feat: update test cases

--- langchain4j-spring-boot-starter/src/test/java/dev/langchain4j/service/spring/mode/automatic/issue3074/TestAutowireAiServiceToolProviderApplication.java
+++ langchain4j-spring-boot-starter/src/test/java/dev/langchain4j/service/spring/mode/automatic/issue3074/TestAutowireAiServiceToolProviderApplication.java
@@ -1,13 +1,26 @@
 package dev.langchain4j.service.spring.mode.automatic.issue3074;
 
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.service.tool.ToolProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
+
 
 @SpringBootApplication
 public class TestAutowireAiServiceToolProviderApplication {
+
+    @Bean
+    ChatModel chatModel() {
+        return OpenAiChatModel.builder()
+                .apiKey(System.getenv("OPENAI_API_KEY"))
+                .modelName(GPT_4_O_MINI)
+                .build();
+    }
+
     @Bean
     public ToolProvider toolProvider() {
         return new TestMcpToolProvider();
--- langchain4j-spring-boot-starter/src/test/java/dev/langchain4j/service/spring/mode/automatic/issue3074/TestAutowrieToolProvider.java
+++ langchain4j-spring-boot-starter/src/test/java/dev/langchain4j/service/spring/mode/automatic/issue3074/TestAutowrieToolProvider.java
@@ -1,10 +1,20 @@
 package dev.langchain4j.service.spring.mode.automatic.issue3074;
 
+import dev.langchain4j.service.AiServiceContext;
+import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.spring.AiServicesAutoConfig;
+import dev.langchain4j.service.tool.ToolProvider;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.Advised;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 public class TestAutowrieToolProvider {
 
@@ -16,7 +26,33 @@ public class TestAutowrieToolProvider {
         contextRunner
                 .withUserConfiguration(TestAutowireAiServiceToolProviderApplication.class)
                 .run(context -> {
-                    Assertions.assertDoesNotThrow(() -> context.getBean(TestMcpToolProvider.class));
+                    ToolProvider toolProviderBean = context.getBean(ToolProvider.class);
+                    Assertions.assertNotNull(toolProviderBean, "ToolProvider bean should be present in the context.");
+
+                    AiServiceWithToolProvider aiServiceProxy = context.getBean(AiServiceWithToolProvider.class);
+                    Assertions.assertNotNull(aiServiceProxy, "AiServiceWithToolProvider should be created successfully.");
+
+                    InvocationHandler handler = Proxy.getInvocationHandler(aiServiceProxy);
+                    Assertions.assertNotNull(handler, "InvocationHandler should be present.");
+                    Field this$0 = handler.getClass().getDeclaredField("this$0");
+                    this$0.setAccessible(true);
+                    Object aiServices = this$0.get(handler);
+
+                    Field contextField = aiServices.getClass().getSuperclass().getDeclaredField("context");
+                    contextField.setAccessible(true);
+                    AiServiceContext aiServiceContext = (AiServiceContext) contextField.get(aiServices);
+                    Assertions.assertNotNull(aiServiceContext, "AiServiceContext should be present.");
+
+                    Field aiServiceField = aiServiceContext.getClass().getDeclaredField("toolService");
+                    aiServiceField.setAccessible(true);
+                    Object toolService = aiServiceField.get(aiServiceContext);
+
+                    Field toolProvider = toolService.getClass().getDeclaredField("toolProvider");
+                    toolProvider.setAccessible(true);
+                    Object toolProviderObj = toolProvider.get(toolService);
+                    Assertions.assertInstanceOf(TestMcpToolProvider.class, toolProviderObj, "ToolProvider should be TestMcpToolProvider.");
+
+
                 });
     }
 }
