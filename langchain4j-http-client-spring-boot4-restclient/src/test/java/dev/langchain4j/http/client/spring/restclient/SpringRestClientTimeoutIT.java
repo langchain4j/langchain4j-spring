19efeca253c03ec10aac1ba7a0e926ef446a7039
Dmytro Liubarskyi <ljubarskij@gmail.com>
2026-04-08 12:47:41 +0200

    fixed SpringRestClientTimeoutIT

--- langchain4j-http-client-spring-boot4-restclient/src/test/java/dev/langchain4j/http/client/spring/restclient/SpringRestClientTimeoutIT.java
+++ langchain4j-http-client-spring-boot4-restclient/src/test/java/dev/langchain4j/http/client/spring/restclient/SpringRestClientTimeoutIT.java
@@ -2,7 +2,10 @@ package dev.langchain4j.http.client.spring.restclient;
 
 import dev.langchain4j.http.client.HttpClient;
 import dev.langchain4j.http.client.HttpClientTimeoutIT;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.http.client.ReactorClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 import java.net.SocketTimeoutException;
@@ -17,19 +20,19 @@ class SpringRestClientTimeoutIT extends HttpClientTimeoutIT {
                 SpringRestClient.builder()
                         .restClientBuilder(RestClient.builder().requestFactory(new JdkClientHttpRequestFactory()))
                         .readTimeout(readTimeout)
+                        .build(),
+                SpringRestClient.builder()
+                        .restClientBuilder(RestClient.builder().requestFactory(new HttpComponentsClientHttpRequestFactory()))
+                        .readTimeout(readTimeout)
+                        .build(),
+                SpringRestClient.builder()
+                        .restClientBuilder(RestClient.builder().requestFactory(new ReactorClientHttpRequestFactory()))
+                        .readTimeout(readTimeout)
+                        .build(),
+                SpringRestClient.builder()
+                        .restClientBuilder(RestClient.builder().requestFactory(new SimpleClientHttpRequestFactory()))
+                        .readTimeout(readTimeout)
                         .build()
-//                SpringRestClient.builder()
-//                        .restClientBuilder(RestClient.builder().requestFactory(new HttpComponentsClientHttpRequestFactory()))
-//                        .readTimeout(readTimeout)
-//                        .build()
-//                SpringRestClient.builder()
-//                        .restClientBuilder(RestClient.builder().requestFactory(new ReactorClientHttpRequestFactory()))
-//                        .readTimeout(readTimeout)
-//                        .build()
-//                SpringRestClient.builder()
-//                        .restClientBuilder(RestClient.builder().requestFactory(new SimpleClientHttpRequestFactory()))
-//                        .readTimeout(readTimeout)
-//                        .build()
         );
     }
 
